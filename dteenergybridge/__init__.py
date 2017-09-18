90b21aa2695c67b18f6170062e3ab9f5f5f97222
Kyle Hendricks <khendricks@nutshell.com>
2017-09-18 14:00:22 -0400

    Bump version: 0.1.0 → 0.1.1

--- dteenergybridge/__init__.py
+++ dteenergybridge/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Kyle John Hendricks"""
 __email__ = 'kyle@hendricks.nu'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from dteenergybridge.dteenergybridge import DteEnergyBridge
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='dteenergybridge',
-    version='0.1.0',
+    version='0.1.1',
     description="Library for retrieving data from a DTE Energy Bridge",
     long_description=readme + '\n\n' + history,
     author="Kyle John Hendricks",
