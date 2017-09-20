74ae1740da049b1d20aef3fcaf17f636b19dfb32
Kyle Hendricks <khendricks@nutshell.com>
2017-09-19 23:48:36 -0400

    Fix flake8 issues

--- dteenergybridge/__init__.py
+++ dteenergybridge/__init__.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-
 """Top-level package for dteenergybridge."""
+
+from dteenergybridge.dteenergybridge import DteEnergyBridge # noqa
 
 __author__ = """Kyle John Hendricks"""
 __email__ = 'kyle@hendricks.nu'
 __version__ = '0.1.1'
-
-from dteenergybridge.dteenergybridge import DteEnergyBridge
